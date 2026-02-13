fe0385b9f70fbef08aba14bb22078657d655d364
Gabriela Rubio <gabyrubioa@gmail.com>
2026-02-13 17:52:50 -0600

    Second part for building a form.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,8 +7,17 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form control" />
+        <input id="name" type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
