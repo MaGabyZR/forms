282b7fc61e212f8debe500d6c2d3c57f2a3851b7
Gabriela Rubio <gabyrubioa@gmail.com>
2026-02-16 12:58:26 -0600

    Controlled Components: using the state hook to access input fields.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,59 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
-  //Accesing Input fields.
+  //use the state hook to create an object.
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
+  /*   //Accesing Input fields. Replaced by using the State Hook instead, above. 
   const nameRef = useRef<HTMLInputElement>(null);
   const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const person = { name: "", age: 0 }; //Send this object to the server to be saved.  
   //Handle Form submission.
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     if (nameRef.current !== null) person.name = nameRef.current.value;
     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
-  };
+    }; */
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          //every time the user types a keystroke, we update the name of the person.
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          //every time the user types a keystroke, we update the age of the person.
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
